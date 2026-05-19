40c62899489dfc247fc5381bdfa1b29df0a8b110
David Oesch <david.oesch@gmail.com>
2026-05-19 07:21:16 +0000

    Enhance PyInstaller build by adding hidden imports for requests_negotiate_sspi, sspi, and ssicon

--- pyinstaller_onefile.bat
+++ pyinstaller_onefile.bat
@@ -178,6 +178,9 @@ pyinstaller --noconfirm --console ^
     --hidden-import=utilities.photo_processor ^
     --hidden-import=utilities.kml_generator ^
     --hidden-import=utilities.stac_publisher ^
+    --hidden-import=requests_negotiate_sspi ^
+    --hidden-import=sspi ^
+    --hidden-import=sspicon ^
     --hidden-import=rasterio.serde ^
     --hidden-import=rasterio._shim ^
     --hidden-import=rasterio.sample ^
